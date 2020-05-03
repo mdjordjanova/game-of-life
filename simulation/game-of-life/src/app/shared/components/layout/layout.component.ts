0a42448748af3b27c5c045026a0f40c7ffc93e54
maria <m.djordjanova@gmail.com>
2020-05-04 01:24:59 +0200

    cleanup; remove unused code

--- simulation/game-of-life/src/app/shared/components/layout/layout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'app-layout',
-  styleUrls: ['./layout.component.scss'],
-  template: ``
-})
-export class LayoutComponent { }
